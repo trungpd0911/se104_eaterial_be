6bf28b776bedeaf7a0a48636e5c1790c64b04e25
unknown <truongthienloc2@gmail.com>
2023-12-20 14:54:05 +0700

    Table - Create socket io for reservation

--- src/server.js
+++ src/server.js
@@ -4,28 +4,37 @@ const initWebRoutes = require('./routes/web');
 const cookieParser = require('cookie-parser');
 const routeTableBooking = require('./routes/tableBookingRoutes');
 const { WebSocketServer } = require('ws');
-const cors = require('cors')
+const cors = require('cors');
 require('dotenv').config();
+const { Server } = require('socket.io');
+const { configSocketIOAuth } = require('./config/socket.io');
+const { configSocketIORoute } = require('./routes/tableBookingSocket');
 
 let app = express();
 
-
-//config app 
+//config app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors())
-
+app.use(cors());
 
 initWebRoutes(app);
 
 let port = process.env.PORT || 3000;
 
-const wsServer = new WebSocketServer({ 
-    server: app.listen(port, () => {
-        console.log(`App is running at the port ${port}`);
-    }), 
-    path: '/table-booking'
+const server = app.listen(port, () => {
+    console.log(`App is running at the port ${port}`);
 });
 
-routeTableBooking(wsServer);
+const io = new Server(server, { cors: { origin: '*' } });
+configSocketIOAuth(io);
+configSocketIORoute(io);
+
+// const wsServer = new WebSocketServer({
+//     server: app.listen(port, () => {
+//         console.log(`App is running at the port ${port}`);
+//     }),
+//     path: '/table-booking'
+// });
+
+// routeTableBooking(wsServer);
